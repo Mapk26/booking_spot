3b8608382d479be535050fb742e0a4b15eb09e9d
Marco Nicotra <marco.nicotra@remedia.it>
2023-10-25 13:25:42 +0400

    Modificato colore optioned

--- lib/src/models/spot_enum.dart
+++ lib/src/models/spot_enum.dart
@@ -9,7 +9,7 @@ enum SpotStatus {
   occupied(Color(0xffEB4444)),
   reserved(Color(0xFF932A2A)),
   partiallyFree(Color(0xffFFB703)),
-  optioned(Color(0xffDFC2F2)),
+  optioned(Color(0xff7F39FB)),
   overbooking(Color(0xFF344055));
   
   const SpotStatus(this.color);
